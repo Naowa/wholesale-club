6091c52b3dc8f5b1b19505d3dd2062f25058edba
frimbraza <ha.kit.chan@gmail.com>
2017-04-06 16:00:29 -0700

    Update member.h

--- Super Warehouse Store Group Project/Super-Warehouse-Store-Group-Project/member.h
+++ Super Warehouse Store Group Project/Super-Warehouse-Store-Group-Project/member.h
@@ -63,6 +63,9 @@ public:
     inline void SetType(Membership new_type) { this->info.type = new_type;}
     inline void ChangeName(const std::string &new_name) { this->info.name = new_name;}
     inline void setExpDate(const std::string &expDate) {this->info.expDate = expDate;} //expDate: MM/DD/YYYY
+    
+    void operator =(Member &other); //Added by Kit 4/6/2017
+    Purchase*& PurchaseHistory();   //Added by Kit 4/6/2017
 
     ~Member();
 private:
